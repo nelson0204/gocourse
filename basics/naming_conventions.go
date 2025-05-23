240ca209909c69fe54ae6d34935c47af1a820e85
nelsonjr <49343425+nelson0204@users.noreply.github.com>
2025-05-23 11:07:35 +0100

    Hello to NaminConv

---
+++ basics/naming_conventions.go
@@ -0,0 +1,23 @@
+package basics
+
+import "fmt"
+
+func main() {
+	// PascalCase
+	// Eg. CalculateArea, UserInfo, NewHTTPRequest
+	// Structs, interfaces, enums
+
+	// snake_case
+	// Eg. user_id, first_name, http_request
+
+	// UPPERCASE
+	// User case in Constants
+
+	// mixedCase
+	// Eg. javaScript, htmlDocument, isValid
+
+	const MAXRETRIES = 5
+
+	var employeeID = 1001
+	fmt.Println("EmployeeID: ", employeeID)
+}
