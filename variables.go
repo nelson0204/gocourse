02e3ee38c90662eb77dd26a15322e2a892d506df
nelsonjr <49343425+nelson0204@users.noreply.github.com>
2025-05-23 10:52:36 +0100

    first commit

---
+++ variables.go
@@ -0,0 +1,19 @@
+package main
+
+func main() {
+	var age int
+	var name string = "john"
+	var name1 = "Jane"
+
+	count := 10
+	lasName := "Smith"
+
+	// Default Values
+	// Numeri types: 0
+	// Boolean types: false
+	// String type: ""
+	// Pointer, Slices, Maps, Functions and Structs: nil
+
+	// --- Scope
+
+}
